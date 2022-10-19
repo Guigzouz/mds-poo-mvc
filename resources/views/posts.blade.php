2adb6e6e952970536296183188bd9b602645cde4
Guillaume Benard <guillaume.benard0@gmail.com>
2022-10-19 12:03:21 +0200

    ajout de posts et gestions de posts +404 page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Posts</title>
+</head>
+<body>
+    <h1>My posts</h1>
+    <div style="display: flex; flex-direction:column">
+        <a href="/posts/my-first-post">My first post</a>
+        <a href="/posts/my-second-post">My second post</a>
+        <a href="/posts/my-third-post">My third post</a>
+    </div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/posts', function () {
+    return view('posts');
+});
+
+Route::get('/posts/{post_name}', function ($post_name) {
+
+
+    $file_name = __DIR__. '/../resources/posts/' . $post_name . '.html';
+        if(!file_exists($file_name)){
+            //return 404
+            return abort(404);
+        }
+    $post_content = file_get_contents($file_name);
+    return view('post', [
+        'post_content' => $post_content
+    ]);
+});
+
